67599cd3f6a31de150b1c250865ce5c24fd486d3
Shrutika Bagul <86190420+Shrutikabagul@users.noreply.github.com>
2024-07-03 00:14:13 +0530

    Update AuthPage.jsx

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -5,7 +5,7 @@ const AuthPage = (props) => {
       e.preventDefault();
       const { value } = e.target[0];
       axios
-        .post("http://localhost:3001/authenticate", { username: value })
+        .post("https://quicktalk-nodebased-chat-app-backend.onrender.com/authenticate", { username: value })
         .then((r) => props.onAuth({ ...r.data, secret: value }))
         .catch((e) => console.log("Auth Error", e));
     };
